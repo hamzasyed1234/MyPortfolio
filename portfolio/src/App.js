ad64d6d5c7a10d044bd3294129f1c69852779401
Hamza Syed <hamzasy416@gmail.com>
2025-09-27 23:49:58 -0400

    Add components

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main className="main-content">
+        <h1>Welcome to My React App</h1>
+        <p>Start building your amazing application!</p>
+      </main>
     </div>
   );
 }
